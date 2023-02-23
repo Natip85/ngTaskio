51addabcb12fadff41a9d0a711935afa6ffc38b9
Nati Peretz <nati.peretz1985@gmail.com>
2023-02-23 14:11:32 +0200

    changed to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngTaskio';
+
+  today = new Date()
+
+
+  getImagePath(): string {
+    return "https://cdn.pixabay.com/photo/2023/02/06/01/14/superb-fairywren-7770904_960_720.jpg"
+  }
+
+  getTaskById(id: number): string {
+    const tasks = [
+      'Your first task', 
+      'Asecond task', 
+      'Another task'
+    ]
+
+    return tasks[id]
+  }
 }
